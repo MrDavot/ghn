ead689c9f8e22fabf9e123fc767f797c129ed49c
MrDavot <36482903+MrDavot@users.noreply.github.com>
2023-10-06 18:47:33 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-x = float(input("Введите число: "))
-
-if x > 0:
-    result = 1
-elif x < 0:
-    result = -1
+x1 = int(input("Введите номер (от 1 до 8): "))
+y1 = int(input("Введите номер (от 1 до 8): "))
+x2 = int(input("Введите номер (от 1 до 8): "))
+y2 = int(input("Введите номер (от 1 до 8): "))
+if (x1 + y1) % 2 == (x2 + y2) % 2:
+    result = "YES"
 else:
-    result = 0
+    result = "NO"
 
-print("Знак числа:", result)
+print("Клетки покрашены в один цвет:", result)
